80276e44fa9df8b6ac3d632b631435bcedac7743
Sebastien Charron <sebcharron0@gmail.com>
2026-01-16 05:11:08 +0000

    fix(monaco): Simplify worker creation by removing redundant function

--- apps/frontend/src/renderer/main.tsx
+++ apps/frontend/src/renderer/main.tsx
@@ -15,29 +15,22 @@ loader.config({ monaco });
 // @ts-ignore - MonacoEnvironment is a global
 self.MonacoEnvironment = {
   getWorker(_: string, label: string) {
-    const getWorkerModule = (moduleUrl: string, label: string) => {
-      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
-        name: label,
-        type: 'module'
-      });
-    };
-
     switch (label) {
       case 'json':
-        return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
+        return new Worker(new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url), { type: 'module' });
       case 'css':
       case 'scss':
       case 'less':
-        return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
+        return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url), { type: 'module' });
       case 'html':
       case 'handlebars':
       case 'razor':
-        return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
+        return new Worker(new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url), { type: 'module' });
       case 'typescript':
       case 'javascript':
-        return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
+        return new Worker(new URL('monaco-editor/esm/vs/language/typescript/ts.worker', import.meta.url), { type: 'module' });
       default:
-        return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
+        return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url), { type: 'module' });
     }
   }
 };
